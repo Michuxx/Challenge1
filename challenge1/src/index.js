4044a1055bdfd63fb57a7758f6e0e67ff7985779
Michuxx <104158118+Michuxx@users.noreply.github.com>
2023-09-03 20:23:20 +0200

    some fixes v2

--- challenge1/src/index.js
+++ challenge1/src/index.js
@@ -9,8 +9,8 @@ import SucccessedApp from './SuccessedApp';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    {/* <App /> */}
-    <SucccessedApp />
+    <App />
+    {/* <SucccessedApp /> */}
   </React.StrictMode>
 );
 
